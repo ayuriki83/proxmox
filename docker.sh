5ca90e9ac82e6490d35b1c6491d5a74bedbdb672
ayuriki83 <ayuriki@naver.com>
2025-09-12 02:48:46 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 2:45
+# 2:49
 # 자동화 스크립트 (INI 스타일 NFO 대응)
 # - NFO 사용자정의 마커(__DOCKER__, __COMMAND__, etc) 직접 파싱
 # - 환경변수 ##KEY## 형식 치환
@@ -120,18 +120,20 @@ run_commands() {
   echo
   echo "=== 실행: $svc ==="
 
+  # 명령어 전체 블록을 줄바꿈 포함하여 하나의 배열 요소로 파싱
   mapfile -t commands < <(
     awk -v svc="$svc" '
       index($0, "__DOCKER__ name=\""svc"\"") > 0 {in_docker=1; next}
       in_docker && $0 ~ /^__COMMANDS_START__$/ {in_cmds=1; next}
       in_docker && $0 ~ /^__COMMANDS_END__$/ {in_cmds=0; next}
       in_docker && in_cmds && $0 ~ /^__COMMAND_START__$/ {in_cmd=1; cmd=""; next}
-      in_docker && in_cmds && $0 ~ /^__COMMAND_END__$/ {if(cmd){print cmd}; cmd=""; in_cmd=0; next}
-      in_docker && in_cmds && in_cmd && $0 !~ /^__/ {cmd=cmd $0 "\n"}
+      in_docker && in_cmds && $0 ~ /^__COMMAND_END__$/ {if(in_cmd){gsub(/\r$/, "", cmd); print cmd}; cmd=""; in_cmd=0; next}
+      in_docker && in_cmds && in_cmd {cmd=cmd $0 "\n"}
       $0 ~ /^__DOCKER_END__$/ {in_docker=0}
-      END{if(cmd!="") print cmd}
+      END{if(cmd!="") {gsub(/\r$/, "", cmd); print cmd}}
     ' "$NFO_FILE"
   )
+
 
   for cmd in "${commands[@]}"; do
     tmpf=$(mktemp)
